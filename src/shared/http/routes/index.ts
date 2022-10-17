2ebfb46ccd9c9466b7f6694cc428e7e23e0c6eb0
nicolas <nicolas.braga@tarkuss.com.br>
2022-10-17 10:48:19 -0400

    [CREATED] roles router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,12 +1,15 @@
-import { AppError } from '../errors/AppError'
+//import { AppError } from '../errors/AppError'
 import { Router } from 'express'
+import { rolesRouter } from '@roles/http/routes/roles.routes'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  throw new AppError('Acesso negado', 401)
+  //throw new AppError('Acesso negado', 401)
 
   return res.json({ message: 'Olá Dev!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
