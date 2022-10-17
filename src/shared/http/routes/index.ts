cc3b803546222caa8a66246821b6779e92741f59
nicolas <nicolas.braga@tarkuss.com.br>
2022-10-17 10:21:25 -0400

    [CREATED] Criando e configurando class para tratamento de erros

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
+import { AppError } from '../errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
+  throw new AppError('Acesso negado', 401)
+
   return res.json({ message: 'Olá Dev!' })
 })
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
-import express = require('express')
+import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cors = require('cors')
 import { routes } from './routes'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
@@ -10,6 +11,20 @@ app.use(express.json())
 app.use(cors())
 
 app.use(routes)
+
+//Midllewares
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message })
+  }
+  console.log(error)
+  return express.response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}!`)
